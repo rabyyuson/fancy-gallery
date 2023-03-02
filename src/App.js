0c19304df5590b829701c29448aa9aea869560a3
Raby Yuson <rabyyuson@gmail.com>
2023-03-02 18:55:28 -0500

    Add fancy gallery submodule

--- src/App.js
+++ src/App.js
@@ -1,24 +1,130 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="page">
+        <div id="main">
+          <div id="primary">
+            <div id="single-loop">
+              <div id="content">
+                <div id="gallery-1" className="gallery galleryid-4 gallery-columns-3 gallery-size-thumbnail"><dl className="gallery-item">
+                  <dt className="gallery-icon landscape">
+                    <a href="/lib/sample_images/1.jpg" title="Lightning storm"><img width="220" height="220" src="/lib/sample_images/thumbnails/1.jpg" className="attachment-thumbnail" alt="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."/></a>
+                  </dt>
+                    <dd className="wp-caption-text gallery-caption">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.
+                    </dd></dl><dl className="gallery-item">
+                  <dt className="gallery-icon landscape">
+                    <a href="/lib/sample_images/2.jpg" title="Akame Shijyuhachi Waterfall, Japan"><img width="220" height="220" src="/lib/sample_images/thumbnails/2.jpg" className="attachment-thumbnail" alt="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."/></a>
+                  </dt>
+                    <dd className="wp-caption-text gallery-caption">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.
+                    </dd></dl><dl className="gallery-item">
+                  <dt className="gallery-icon landscape">
+                    <a href="/lib/sample_images/3.jpg" title="Coyote mother and pup"><img width="220" height="220" src="/lib/sample_images/thumbnails/3.jpg" className="attachment-thumbnail" alt="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."/></a>
+                  </dt>
+                    <dd className="wp-caption-text gallery-caption">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.
+                    </dd></dl>
+                  <dt className="gallery-icon landscape">
+                    <a href="/lib/sample_images/4.jpg" title="Flamingo"><img width="220" height="220" src="/lib/sample_images/thumbnails/4.jpg" className="attachment-thumbnail" alt="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."/></a>
+                  </dt>
+                    <dd className="wp-caption-text gallery-caption">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.
+                    </dd><dl className="gallery-item">
+                  <dt className="gallery-icon landscape">
+                    <a href="/lib/sample_images/5.jpg" title="Venetian mask among bright blue feathers."><img width="220" height="220" src="/lib/sample_images/thumbnails/5.jpg" className="attachment-thumbnail" alt="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."/></a>
+                  </dt>
+                    <dd className="wp-caption-text gallery-caption">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.
+                    </dd></dl><dl className="gallery-item">
+                  <dt className="gallery-icon landscape">
+                    <a href="/lib/sample_images/6.jpg" title="Kihei, Maui (Hawaii, USA)"><img width="220" height="220" src="/lib/sample_images/thumbnails/6.jpg" className="attachment-thumbnail" alt="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."/></a>
+                  </dt>
+                    <dd className="wp-caption-text gallery-caption">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.
+                    </dd></dl>
+                  <dt className="gallery-icon landscape">
+                    <a href="/lib/sample_images/7.jpg" title="Red Hot Chili Pepper, Kerala, India"><img width="220" height="220" src="/lib/sample_images/thumbnails/7.jpg" className="attachment-thumbnail" alt="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."/></a>
+                  </dt>
+                    <dd className="wp-caption-text gallery-caption">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.
+                    </dd><dl className="gallery-item">
+                  <dt className="gallery-icon landscape">
+                    <a href="/lib/sample_images/8.jpg" title="Gamlehaugen at sunset, Norway"><img width="220" height="220" src="/lib/sample_images/thumbnails/8.jpg" className="attachment-thumbnail" alt="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."/></a>
+                  </dt>
+                    <dd className="wp-caption-text gallery-caption">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.
+                    </dd></dl><dl className="gallery-item">
+                  <dt className="gallery-icon landscape">
+                    <a href="/lib/sample_images/9.jpg" title="Gray wolf in wildlife sanctuary"><img width="220" height="220" src="/lib/sample_images/thumbnails/9.jpg" className="attachment-thumbnail" alt="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."/></a>
+                  </dt>
+                    <dd className="wp-caption-text gallery-caption">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.
+                    </dd></dl>
+                  <dt className="gallery-icon landscape">
+                    <a href="/lib/sample_images/10.jpg" title="Tiger Cub"><img width="220" height="220" src="/lib/sample_images/thumbnails/10.jpg" className="attachment-thumbnail" alt="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."/></a>
+                  </dt>
+                    <dd className="wp-caption-text gallery-caption">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.
+                    </dd><dl className="gallery-item">
+                  <dt className="gallery-icon landscape">
+                    <a href="/lib/sample_images/11.jpg" title="Purple flowers in a backyard in Madrid, Spain"><img width="220" height="220" src="/lib/sample_images/thumbnails/11.jpg" className="attachment-thumbnail" alt="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."/></a>
+                  </dt>
+                    <dd className="wp-caption-text gallery-caption">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.
+                    </dd></dl><dl className="gallery-item">
+                  <dt className="gallery-icon landscape">
+                    <a href="/lib/sample_images/12.jpg" title="Lightning bolts in night sky"><img width="220" height="220" src="/lib/sample_images/thumbnails/12.jpg" className="attachment-thumbnail" alt="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."/></a>
+                  </dt>
+                    <dd className="wp-caption-text gallery-caption">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.
+                    </dd></dl>
+                  <dt className="gallery-icon landscape">
+                    <a href="/lib/sample_images/13.jpg" title="Slunecnice"><img width="220" height="220" src="/lib/sample_images/thumbnails/13.jpg" className="attachment-thumbnail" alt="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."/></a>
+                  </dt>
+                    <dd className="wp-caption-text gallery-caption">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.
+                    </dd><dl className="gallery-item">
+                  <dt className="gallery-icon landscape">
+                    <a href="/lib/sample_images/14.jpg" title="Chmírí pampeliky"><img width="220" height="220" src="/lib/sample_images/thumbnails/14.jpg" className="attachment-thumbnail" alt="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."/></a>
+                  </dt>
+                    <dd className="wp-caption-text gallery-caption">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.
+                    </dd></dl><dl className="gallery-item">
+                  <dt className="gallery-icon landscape">
+                    <a href="/lib/sample_images/15.jpg" title="Rainbow Lorikeet (Trichoglossus haematodus), Sydney, Australia"><img width="220" height="220" src="/lib/sample_images/thumbnails/15.jpg" className="attachment-thumbnail" alt="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."/></a>
+                  </dt>
+                    <dd className="wp-caption-text gallery-caption">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.
+                    </dd></dl>
+                  <dt className="gallery-icon landscape">
+                    <a href="/lib/sample_images/16.jpg" title="Plant in a cafes garden"><img width="220" height="220" src="/lib/sample_images/thumbnails/16.jpg" className="attachment-thumbnail" alt="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."/></a>
+                  </dt>
+                    <dd className="wp-caption-text gallery-caption">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.
+                    </dd><dl className="gallery-item">
+                  <dt className="gallery-icon landscape">
+                    <a href="/lib/sample_images/17.jpg" title="Hanna Highway, Maui, Hawaii, United States"><img width="220" height="220" src="/lib/sample_images/thumbnails/17.jpg" className="attachment-thumbnail" alt="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."/></a>
+                  </dt>
+                    <dd className="wp-caption-text gallery-caption">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.
+                    </dd></dl><dl className="gallery-item">
+                  <dt className="gallery-icon landscape">
+                    <a href="/lib/sample_images/18.jpg" title="Dandelion"><img width="220" height="220" src="/lib/sample_images/thumbnails/18.jpg" className="attachment-thumbnail" alt="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."/></a>
+                  </dt>
+                    <dd className="wp-caption-text gallery-caption">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.
+                    </dd></dl>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
